c4a4eab318050660637016918ae230afdcef5974
bhuvaneswaranm <bhuvaneswaranm@PRINHYLTPAP0474.local>
2015-05-25 13:13:21 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ var app=angular.module("App",[]);
 app.controller("ModalController",["$scope",function($scope){
 
 	$scope.openModal=true;
+	$scope.closeModal=function(event){
+		debugger;
+		if(event.target==event.currentTarget){
+			$scope.openModal=false;
+		}
+	}
 }])
